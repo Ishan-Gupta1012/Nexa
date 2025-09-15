b7f3e1c6eb3027605c15d8853903ab8d990057c3
ishan-gupta1012 <ishangupta1012@gmail.com>
2025-09-15 14:05:22 +0530

    A brief, clear message about what you changed

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,13 @@ import InterviewPrep from './pages/InterviewPrep.jsx';
 import Strategies from './pages/Strategies.jsx';
 
 export const routes = [
-  // { path: "/signin", element: <SignIn /> },
+  { path: "/signin", element: <SignIn /> },
   { path: "/resume-preview", element: <ResumePreview /> },
   {
     path:"/",
-    element: <SignIn />,
+    element: <Layout />,
     children: [
-      // { index: true, element: <Dashboard /> },
+      { index: true, element: <Dashboard /> },
       { path: "/dashboard", element: <Dashboard /> },
       { path: "/profile", element: <Profile /> },
       { path: "/resume-builder", element: <ResumeBuilder /> },
