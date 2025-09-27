ae4ff6c111013af20d5cf1a1ba96faeb8bbcb22e
ishan-gupta1012 <ishangupta1012@gmail.com>
2025-09-27 22:22:49 +0530

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
-import { routes } from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./App.jsx";
+import "./index.css";
 
 // 1. Define the future flags to opt into new behaviors and remove warnings
 const future = {
-  v7_startTransition: true,
-  v7_relativeSplatPath: true,
+	v7_startTransition: true,
+	v7_relativeSplatPath: true,
 };
 
 // 2. Create the router using the new data router API and pass the flags
-const router = createHashRouter(routes, { future });
+const router = createBrowserRouter(routes, { future });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
+
